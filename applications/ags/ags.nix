c34666e0606501880c5769283065ca28ec15c557
Baneet Parmar <baneetparmar7@gmail.com>
2024-06-01 20:53:11 +0530

    more configs to hm

---
+++ applications/ags/ags.nix
@@ -0,0 +1,14 @@
+{ pkgs, ags, ... }: {
+  # add the home manager module
+  imports = [ ags.homeManagerModules.default ];
+
+  programs.ags = {
+    enable = true;
+
+    # null or path, leave as null if you don't want hm to manage the config
+    configDir = ../ags/;
+
+    # additional packages to add to gjs's runtime
+    extraPackages = with pkgs; [ gtksourceview webkitgtk accountsservice ];
+  };
+}
