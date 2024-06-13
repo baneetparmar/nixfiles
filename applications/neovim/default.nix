10e417b7a6004bfe8bd4656fd55930f4a461d0f5
Baneet Parmar <baneetparmar7@gmail.com>
2024-06-13 18:15:54 +0530

    neovim config

--- applications/neovim/default.nix
+++ applications/neovim/default.nix
@@ -1,11 +1,83 @@
-{ inputs, ... }:
+{ pkgs, ... }:
 
+let
+  toLua = str: "lua << EOF\n${str}\nEOF\n";
+  loadConf = pluginName: "${builtins.readFile ./plugins/${pluginName}.lua}";
+
+in
 {
-  imports = [ inputs.nixvim.homeManagerModules.nixvim ./keymaps.nix ./plugins ./options.nix ];
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
 
+    plugins = with pkgs.vimPlugins; [
 
-  home.shellAliases.nv = "nvim";
+      # ==== lsp and auto-completion ==== #
+      luasnip
+      nvim-cmp
+      cmp-path
+      cmp-buffer
+      fidget-nvim
+      cmp-cmdline
+      cmp_luasnip
+      cmp-nvim-lsp
+      mason-nvim
+      mason-lspconfig-nvim
 
-  programs.nixvim.enable = true;
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = loadConf "nvim-lspconfig";
+      }
 
+      # ==== eye candy ==== #
+      {
+        plugin = nvim-treesitter.withAllGrammars;
+        type = "lua";
+        config = loadConf "nvim-treesitter";
+      }
+
+      {
+        plugin = tokyonight-nvim;
+        type = "lua";
+        config = loadConf "theme";
+      }
+
+      # ==== utils ===== #
+      {
+        plugin = telescope-nvim;
+        type = "lua";
+        config = loadConf "telescope";
+      }
+
+      {
+        plugin = harpoon;
+        type = "lua";
+        config = loadConf "harpoon";
+      }
+
+      {
+        plugin = trouble-nvim;
+        type = "lua";
+        config = loadConf "trouble-nvim";
+      }
+
+      {
+        plugin = undotree;
+        type = "lua";
+        config = loadConf "undotree";
+      }
+    ];
+
+    extraPackages = with pkgs; [
+    ];
+
+    extraConfig = ''
+      luafile /home/bane/.nixfiles/applications/neovim/options.lua
+      luafile /home/bane/.nixfiles/applications/neovim/autocmds.lua
+      luafile /home/bane/.nixfiles/applications/neovim/keymaps.lua
+    '';
+  };
 }
--- justfile
+++ justfile
@@ -1,27 +1,39 @@
+set quiet
+
 add:
   just format
+  @echo "Adding git files...."
   git add .
 
 #updates user packages via home-manager
 dup:
   just add
+  @echo "Updating home-manager...."
   home-manager switch --flake ~/.nixfiles
+  @echo "Done!...."
 
 # same as dup but backups configs before overwriting
 dupb:
   just add
+  @echo "Backing up configs.... \n Done!. \n Updating home-manager...."
   home-manager switch -b backup --flake ~/.nixfiles 
+  @echo "Done!...."
 
 [no-cd]
 format:
+  @echo "Formatting files...."
   nixpkgs-fmt ~/.nixfiles
-  npx prettier --ignore-unknown --write "/home/bane/.nixfiles/**/*.{css,js,ts,json,yaml,toml}"
+  npx prettier --ignore-unknown --log-level warn --write "/home/bane/.nixfiles/**/*.{css,js,ts,json,yaml,toml}"
+  @echo "Done!...."
 
 commit message="update":
   just add
+  @echo "Commiting git files...."
   git commit -m "{{message}}"
 
 push message="update":
   just commit "{{message}}"
+  @echo "Pushing git files...."
   git push
+  @echo "Done!..."
 
