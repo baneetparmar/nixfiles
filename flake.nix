c09db3252a2536240d1d6510c785d28e75c182fb
Baneet Parmar <baneetparmar7@gmail.com>
2025-09-08 13:42:44 +0530

    📌 Pin Hyprland to v0.50.1

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     hyprcursor-phinger.url = "github:Jappie3/hyprcursor-phinger";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-    hyprland.url = "github:hyprwm/Hyprland?ref=314a0ea441e33122836965c50d4c5bcf9acd0cdd";
+    hyprland.url = "github:hyprwm/Hyprland?ref=v0.50.1";
     hypr-darkwindow = {
       url = "github:micha4w/Hypr-DarkWindow";
       inputs.hyprland.follows = "hyprland";
